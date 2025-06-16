67a0766ccc4d5d71b9fb5747ef442d789350087e
anabananaofc <143606711+anabananaoofc@users.noreply.github.com>
2025-06-16 09:01:50 -0300

    aula 4 16/06 09:01

---
+++ main.js
@@ -0,0 +1,23 @@
+const numeroSenha = document.querySelector('.parametro-senha_texto');
+let tamanho = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha_botao');
+
+botoes[0].onclick = diminuiTamanho
+botoes[1].onclick = aumentaTamanho
+
+function diminuiTamanho() {
+    if (tamanhoSenha > 1){
+        tamanhoSenha = tamanhoSenha -1;
+  }
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+function aumentaTamanho() {
+    if (tamanhoSenha < 20){
+        tamanosenhaha = tamanhoSenha +1;
+
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
